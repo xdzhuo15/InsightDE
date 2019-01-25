efe160286565d4ee8bdf95b402af2eea953b76e5
Xiaoding Zhuo <xdzhuoyy@gmail.com>
2019-01-24 22:03:16 -0800

    Update kafka_producer.py

--- kafka_producer.py
+++ kafka_producer.py
@@ -16,12 +16,12 @@ producer = KafkaProducer(bootstrap_servers = ['localhost:9092'])
 
 #creates bucket that points to data
 s3 = boto3.resource('s3', aws_access_key_id = 'AWS_ACCESS_KEY_ID', aws_secret_access_key = 'AWS_SECRET_ACCESS_KEY')
-s3_client = boto3.client('s3')
-data = s3_client.get_object(Bucket='microsoftpred', Key='test.csv')
-#bucket = s3.Bucket('microsoftpred')
-#obj = bucket.Object(Key='test.csv')
+bucket = s3.Bucket('microsoftpred')
+for obj in bucket.objects.all():
+    if obj.key == 'test.csv':
+        test_obj = obj
 
-data = pd.read_csv(obj)
+data = pd.read_csv(test_obj)
 
 #read through each line of csv and send the line to the kafka topic
 for index, row in data.iterrows():
