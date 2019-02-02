fab0b7762f2d57c348c8d59a0be4e5b7beeac0df
Ubuntu <ubuntu@ip-10-0-0-13.us-west-2.compute.internal>
2019-02-02 06:09:34 +0000

    almost fixed streaming json

--- test/spark_stream_test.py
+++ test/spark_stream_test.py
@@ -15,22 +15,46 @@ from pyspark.sql import SQLContext, SparkSession, Row, Column
 from pyspark.sql.types import *
 import json
 from test_schema import Schema
+
+def handler(message):
+    records = message.collect()
+    len(records)
+    
+
+def convert_json2df(rdd,Schema):
+    ss = SparkSession(rdd.context)
+    if rdd.isEmpty():
+        return
+    df = ss.createDataFrame(rdd, schema=Schema)
+    df.show()    
+   
+
 conf = SparkConf().setAppName("prediction").setMaster(
         "spark://ec2-52-10-44-193.us-west-2.compute.amazonaws.com:7077"
         )
 
 sc = SparkContext(conf=conf)
-#sc.setLogLevel("WARN")
+sc.setLogLevel("WARN")
 
 ssc = StreamingContext(sc, 5)
+#set up check point
+#ssc.checkpoint(checkpointDirectory)
 
 kafka_stream = KafkaUtils.createDirectStream(ssc, 
     ["DeviceRecord"], {"metadata.broker.list":"ip-10-0-0-7:9092,ip-10-0-0-11:9092,ip-10-0-0-10:9092"})
 #lines = kafka_stream.map(lambda x: x[1])
 
-kafka_stream = kafka_stream.map(lambda x: x.decode("utf-8"))
-print 'Event recieved in window!!!!!!: ', kafka_stream.pprint()
+#kafka_stream = kafka_stream.map(lambda x: x.decode("utf-8"))
 
+kafka_stream = kafka_stream.map(lambda (key, value): json.loads(value))
+
+cols=["MachineIdentifier","EngineVersion","AvSigVersion","IsBeta","CityIdentifier"]
+
+kafka_stream.foreachRDD(lambda x: convert_json2df(x, cols))
+
+#kafka_stream.foreachRDD(handler)
+print 'Event recieved in window!!!!!!: ', kafka_stream.pprint()
+#kafka_stream.select('AvSigVersion','IsBeta','CityIdentifier').show()
 #df = sc.createDataFrame(kafka_stream)
 #kafka_stream = ssc.union(kafka_stream)
 
