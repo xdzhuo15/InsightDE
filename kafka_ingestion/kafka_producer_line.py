f7902a0fb049884cd137c4008de60013a793db18
Ubuntu <ubuntu@ip-10-0-0-11.us-west-2.compute.internal>
2019-02-06 04:50:08 +0000

    kafka fix

--- kafka_ingestion/kafka_producer_line.py
+++ kafka_ingestion/kafka_producer_line.py
@@ -4,15 +4,14 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
+from smart_open import smart_open
 from kafka import KafkaProducer
 import time
-import smart_open
 
 if __name__ == "__main__":  
     file_name = "test_2000.csv"
     producer = KafkaProducer(bootstrap_servers = "localhost:9092")    
-    for line in smart_open(file_name):
+    for line in smart_open("s3://microsoftpred/test_2000.csv"):
         producer.send('DeviceRecord',line)
         producer.flush()
         time.sleep(3)
