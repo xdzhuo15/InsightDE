202286dced22f8a624a0fd97098fd611b5a7fc6f
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2019-02-09 19:23:58 +0000

    good flask mysql connection

---
+++ test/flask_mysql.py
@@ -0,0 +1,16 @@
+import plotly
+plotly.__version__
+
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+import MySQLdb
+
+conn = MySQLdb.connect(host="ec2-34-211-3-37.us-west-2.compute.amazonaws.com",
+        user="krause", passwd="Insight2019", db="Prediction")
+cursor = conn.cursor()
+cursor.execute('select * FROM R');
+
+rows = cursor.fetchall()
+print(rows)
+
