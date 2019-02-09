b9f30574f60064576e53a2697158e948a97912ca
Xiaoding <xdzhuoyy@gmail.com>
2019-02-09 00:44:52 -0800

    ui draft

--- flask_UI/app.py
+++ flask_UI/app.py
@@ -1,33 +1,58 @@
-# -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+df_test = pd.read_csv(
+    "test_result.csv"
+)
+df_train = pd.read_csv(
+    "test_new.csv"
+)
+feature_options = pd.DataFrame(["SmartScreen","AVProductStatesIdentifier",
+                                "CountryIdentifier", "AVProductsInstalled",
+                                "Census_OSVersion", "EngineVersion",
+                                "AppVersion", "Census_OSBuildRevision",
+                                "GeoNameIdentifier", "OsBuildLab"])
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash()
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
-
+app.layout = html.Div([
+    html.H2("Model Quality Assessment"),
+    html.Div(
+        [
+            dcc.Dropdown(
+                id="Features",
+                options=[{
+                    'label': i,
+                    'value': i
+                } for i in feature_options],
+                value="SmartScreen"),
+        ],
+        style={'width': '25%',
+               'display': 'inline-block'}),
+    dcc.Graph(id='funnel-graph'),
 ])
 
+
+@app.callback(
+    dash.dependencies.Output('funnel-graph', 'figure'),
+    [dash.dependencies.Input('Features', 'value')])
+def update_graph(Feature):
+    df_plot_train = df_train.groupby(Feature).nunique()
+    df_plot_test = df_test.groupby(Feature).nunique()
+
+    data_train = {'x': df_plot_train[:,0], 'y': df_plot_train[:,1], 'type': 'bar', 'name': 'Training Data Distribution'}
+    data_test = {'x': df_plot_test[:,0], 'y': df_plot_test[:,1], 'type': 'bar', 'name': 'Preiction Data Distribution'}
+
+    return {
+        'data': [data_train, data_test],
+        'layout':
+        go.Layout(
+            title='Customer Order Status for {}'.format(Feature))
+    }
+
+
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', debug=True)
+    app.run_server(host='0.0.0.0',debug=True)
