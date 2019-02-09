5b968d353a83e9f5c4c1af7eb90d500cfe6f3976
Ubuntu <ubuntu@ip-10-0-0-13.us-west-2.compute.internal>
2019-02-09 03:58:24 +0000

    sparksql_test

---
+++ test/mysqltest.py
@@ -0,0 +1,13 @@
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SQLContext, SparkSession
+
+spark = SparkSession.builder.appName('ReadData').getOrCreate()
+
+df = spark.read.format("jdbc").options(
+    url='jdbc:mysql://ec2-34-211-3-37.us-west-2.compute.amazonaws.com:3306/Prediction',
+    driver = 'com.mysql.cj.jdbc.Driver',
+    dbtable = 'R',
+    user = 'krause',
+    password = 'Insight2019').load()
+df.show()
+
