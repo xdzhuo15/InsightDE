b394e75399ba183f1aa5bf325111d536ae7e50f2
Ubuntu <ubuntu@ip-10-0-0-13.us-west-2.compute.internal>
2019-02-16 05:20:02 +0000

    new fq

--- Modules/test2.py
+++ Modules/test2.py
@@ -1,31 +1,46 @@
 from freqencoder import FreqEncoder, FreqEncoderModel
 from pyspark.sql import SQLContext, SparkSession
-from pyspark.ml.feature import MinMaxScaler, VectorAssembler
+from pyspark.ml.feature import VectorAssembler
 import pyspark
 from pyspark import SparkConf
 from pyspark.ml import Pipeline
-
+from pyspark.ml.regression import LinearRegression
 
 spark = SparkSession.builder.appName("ReadData").getOrCreate()
 
 data = [
-    ("Tax", 2),
-    ("Tax", 10),
-    ("Food", 13),
-    ("Moon", 5),
-    ("Tax", 8),
-    ("Moon", 7),
-    ("Wind", 1)
+    ("Tax", 2.0),
+    ("Tax", 10.2),
+    ("Food", 13.2),
+    ("Moon", 5.4),
+    ("Tax", 8.6),
+    ("Moon", 7.5),
+    ("Wind", 1.7)
 ]
 
 df = spark.createDataFrame(data, ["cat", "num"])
 
+indexer = FreqEncoder().setInputCol("cat").setOutputCol("count_cat")
+model1 = indexer.fit(df)
+output1 = model1.transform(df)
+output1.show()
+print "showing original dataframe"
+df.show()
+
+lr = LinearRegression(featuresCol = "features_vec", labelCol="HasDetections",maxIter=10 )
+#assembler = VectorAssembler(inputCols =["count_cat","num"], outputCol="feature")
+#model2 = assembler.fit(output1)
+#output2 = model2.transform(output1)
+
+#output2.show()
+#print "showing original dataframe"
+#df.show()
+
 stages = []
 indexer = FreqEncoder().setInputCol("cat").setOutputCol("count_cat")
 stages += [indexer]
-for col in ["num","count_cat"]:
-     scaler = MinMaxScaler().setInputCol(col).setOutputCol(col+"_norm")
-     stages += [stages]
+assembler = VectorAssembler(inputCols=["count_cat","num"],outputCol="feature")
+stages += [assembler]
 pipeline = Pipeline(stages = stages)
 
 model = pipeline.fit(df)
